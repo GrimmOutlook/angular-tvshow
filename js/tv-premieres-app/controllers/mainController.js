2ed83450d0654abd7bb341bcafd060b42e8577fb
GrimmOutlook <grimmoutlook87@gmail.com>
2014-12-27 22:02:59 -0800

    Actually not sure

--- js/tv-premieres-app/controllers/mainController.js
+++ js/tv-premieres-app/controllers/mainController.js
@@ -45,5 +45,23 @@ app.controller("mainController", function($scope, $http){
 });
 
 
+// declares a custom filter to our app called isGenre
+app.filter('isGenre', function() {
+        return function(input, genre) {
+            if (typeof genre == 'undefined' || genre == null) {
+                return input;
+            } else {
+                var out = [];
+                for (var a = 0; a < input.length; a++){
+                    for (var b = 0; b < input[a].show.genres.length; b++){
+                        if(input[a].show.genres[b] == genre) {
+                            out.push(input[a]);
+                        }
+                    }
+                }
+                return out;
+            }
+        };
+    });
 
 
